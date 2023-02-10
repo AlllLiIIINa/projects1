1104fc984edfd9b7380901ee0c7227572f81e805
ALiNa <alina.tvvv@gmail.com>
2023-02-10 18:49:36 +0300

    'hw13'

--- lms_AT/groups/models.py
+++ lms_AT/groups/models.py
@@ -1,3 +1,23 @@
-from django.db import models # noqa
+from datetime import date
 
-# Create your models here.
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+
+class Group(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='name',
+        db_column='name',
+        validators=[MinLengthValidator(2, '"name" field value less than two symbols')]
+    )
+    description = models.TextField(
+        max_length=500,
+        verbose_name='description',
+        db_column='description',
+        validators=[MinLengthValidator(2, '"description" field value less than two symbols')]
+    )
+    start = models.DateField(default=date.today, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} {self.description} {self.start}'
