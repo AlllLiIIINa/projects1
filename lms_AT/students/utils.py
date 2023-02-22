0f3fd5b2b7ae00d9ac025868b21aa69235ecd4e1
ALiNa <alina.tvvv@gmail.com>
2023-02-22 20:29:34 +0200

    'correct'

--- lms_AT/students/utils.py
+++ lms_AT/students/utils.py
@@ -3,7 +3,7 @@ def qs2html(qs_list):
     s = '<table>'
     for student in qs_list:
         s += f'<tr><td>{student.first_name}</td><td>{student.last_name}</td><td>{student.birthday}</td>' \
-             f'<td>{student.email}</td></tr>'
+             f'<td>{student.email}</td><td>{student.phone}</td></tr>'
     s += '</table>'
 
     return s
